471b580e4b85bd7c245e42c7dc15578beea1ecd3
PandaTrumpet <dsavontrumpet@gmail.com>
2024-11-10 21:47:51 +0100

    add coin functional

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -1,6 +1,14 @@
+import { useContext, useEffect, useState } from "react";
 import css from "./Home.module.css";
+import { CoinContext } from "../../context/coinContext.jsx";
 
 const Home = () => {
+  const { allCoin, currency } = useContext(CoinContext);
+  const [displayCoin, setDisplayCoin] = useState([]);
+  useEffect(() => {
+    setDisplayCoin(allCoin);
+  }, [allCoin]);
+
   return (
     <div className={css.home}>
       <div className={css.hero}>
@@ -25,6 +33,22 @@ const Home = () => {
           <p style={{ textAlign: "center" }}>24H Change</p>
           <p className={css.marketCap}>Market Cap</p>
         </div>
+        {displayCoin.slice(0, 10).map((item, index) => (
+          <div className={css.tableLayout} key={index}>
+            <p>{item.market_cap_rank}</p>
+            <div>
+              <img src={item.image} alt="Monet picture" />
+              <p>{item.name + " - " + item.symbol}</p>
+            </div>
+            <p>
+              {currency.symbol} {item.current_price.toLocaleString()}
+            </p>
+            <p>{item.price_change_percentage_24h}</p>
+            <p className={css.marketCap}>
+              {currency.symbol} {item.market_cap.toLocaleString()}
+            </p>
+          </div>
+        ))}
       </div>
     </div>
   );
